ec09bb909fe64410ceaaae6c1fee495bd1546812
Albert Weichselbraun <albert@weichselbraun.net>
2024-03-29 21:47:52 +0100

    wip: wireguard monitor.

--- src/cronitor/monitor/wireguard.py
+++ src/cronitor/monitor/wireguard.py
@@ -1,69 +1,119 @@
 #!/usr/bin/env python3
 """
 Check and repair wireguard interfaces that have been frozen due to a change of the endpoint's IP address.
+
+Logic:
+- get a list of relevant interfaces + their corresponding config
+- read the global wl interface status
+- for each relevant interface;  check if a timeout occurred
+  - no => return none
+  - yes => try updating the endpoint ip, return an info on the update, if successful
+           and a warning otherwise.
 """
 import configparser
+import ipaddress
+import socket
 import subprocess
-from collections import namedtuple
-from glob import glob
-from os.path import basename
 from time import time
-from typing import Optional
+from typing import Optional, NamedTuple
 
 from cronitor import Monitor
 
 WL_HANDSHAKE = ["wg", "show", "all", "latest-handshakes"]
+WL_CHANGE_ENDPOINT = ["wg", "set", "endpoint"]
 
-DEFAULT_CONFIG_DIR = "/etc/wireguard"
 DEFAULT_TIMEOUT = 150
 
-EndPoint = namedtuple("EndPoint", "host port")
+
+class EndPoint(NamedTuple):
+    host: str
+    port: int
+
+
+class WireGuardInterface(NamedTuple):
+    interface_spec: str
+    endpoint: EndPoint
 
 
 class WireguardMonitor(Monitor):
-    def __init__(
-        self, wireguard_config_dir=DEFAULT_CONFIG_DIR, timeout=DEFAULT_TIMEOUT
-    ):
+    def __init__(self, interfaces: dict[str, str], timeout: int = DEFAULT_TIMEOUT):
         self.timeout = timeout
-        self.interfaces = {
-            basename(name).split(".conf")[0]: self.read_host_from_wl_config(name)
-            for name in glob(f"{wireguard_config_dir}/*.conf")
-        }
+        self.interfaces = [
+            WireGuardInterface(
+                interface_spec=interface_spec,
+                endpoint=self.read_host_from_wl_config(interface_conf),
+            )
+            for interface_spec, interface_conf in interfaces.items()
+        ]
 
     def notify(self, force: bool = True) -> str:
         """
-        Check all configured wiregard interfaces and notify the user of frozen ones.
+        Check all configured wireguard interfaces and notify the user of frozen ones.
         """
-
-        if queue_size := self.get_queue_size():
-            return f"# Mail monitoring:\n- {queue_size} mails are currently queued."
-        elif force:
-            return "# Mail monitoring:\n- Mail queue is empty."
-        else:
-            return ""
+        msg = []
+        for interface_spec, endpoint in self.interfaces:
+            namespace, name = interface_spec.split(":")
+            if delta := self.time_since_last_handshake(name, namespace) > self.timeout:
+                msg.append(
+                    f"Wireguard interface {interface_spec} hasn't responded for"
+                    f"{round(delta/60)} seconds."
+                )
+                if not endpoint:
+                    msg.append("Cannot reconnect. No endpoint for interface specified.")
+                self.reconnect_to_wireguard_server(name, namespace, endpoint)
+                if self.time_since_last_handshake(name, namespace) > self.timeout:
+                    msg.append(f"Critical: Reconnect to endpoint {endpoint} failed.")
+                else:
+                    msg.append(f"Successfully reconnected to endpoint {endpoint}.")
+        return "\n".join(msg)
 
     @staticmethod
-    def read_host_from_wl_config(fname: str) -> Optional[EndPoint]:
+    def get_ipaddress(hostname: str) -> Optional[str]:
+        """
+        Return the IP address for the given hostname
+        """
+        try:
+            ipaddress.ip_address(hostname)
+            return hostname
+        except ValueError:
+            pass
+        try:
+            return socket.gethostbyname(hostname)
+        except socket.error as e:
+            print(f"Error: {e}")
+            return None
+
+    @staticmethod
+    def reconnect_to_wireguard_server(
+        name: str, namespace: str, endpoint: EndPoint
+    ) -> None:
+        pass
+
+    @staticmethod
+    def read_host_from_wl_config(config_filename: str) -> Optional[EndPoint]:
         """
         Determine the endpoints for all configured client wireguard interfaces.
         (i.e., only the interfaces that connect to an endpoint).
 
         Args:
-             fname: path to wireguard configuration file.
+             config_filename: path to wireguard configuration file.
 
         Returns:
             An Endpoint tuple which consists of the host and port of the endpoint.
         """
         config = configparser.ConfigParser()
-        with open(fname) as f:
+        with open(config_filename) as f:
             config.read_file(f)
         try:
-            return EndPoint(*config.get("Peer", "Endpoint").split(":"))
+            host, port = config.get("Peer", "Endpoint").split(":")
+            return EndPoint(host, int(port))
         except configparser.NoOptionError:
             return None
 
     @staticmethod
-    def read_wg_status(namespace: str = None):
+    def time_since_last_handshake(
+        interface: str, namespace: Optional[str] = None
+    ) -> float:
         """
         Determine the status of all wireguard connections on the given host.
 
@@ -71,17 +121,21 @@ class WireguardMonitor(Monitor):
         wg-de	KEY=	1708807507
 
         Returns:
-            A list of 'interface' and 'time since last handshake' tuples.
+            The time since last handshake'.
         """
         cmd = (
             WL_HANDSHAKE
             if not namespace
             else ["ip", "netns", "exec", namespace] + WL_HANDSHAKE
         )
-        return [
-            (iface, time() - int(timestamp))
-            for line in subprocess.run(
-                cmd, check=True, capture_output=True, text=True
-            ).stdout.split("\n")
-            for iface, _, timestamp in line.split()
-        ]
+        return next(
+            (
+                time() - float(timestamp)
+                for line in subprocess.run(
+                    cmd, check=True, capture_output=True, text=True
+                ).stdout.split("\n")
+                for iface, _, timestamp in [line.split()]
+                if iface == interface
+            ),
+            None,
+        )
--- tests/test_wireguard.py
+++ tests/test_wireguard.py
@@ -1,20 +1,32 @@
-import gzip
-from datetime import timedelta, datetime
 from os.path import dirname
-
-from cronitor.monitor.borgbackup import BorgBackupMonitor
 from pathlib import Path
+from unittest.mock import patch, MagicMock
 
-from cronitor.monitor.wireguard import WireguardMonitor, EndPoint
+from cronitor.monitor.wireguard import WireguardMonitor, EndPoint, WireGuardInterface
 
-WIREGUARD_CONFIG_DIR = dirname(__file__) + "/data/wireguard"
+WIREGUARD_CONFIG_DIR = Path(dirname(__file__)) / "data" / "wireguard"
 
 
 def test_wireguard_cronitor():
-    wg = WireguardMonitor(wireguard_config_dir=WIREGUARD_CONFIG_DIR)
-    print(wg.interfaces)
-    assert wg.interfaces == {
-        "client1": EndPoint(host="localhost", port="8888"),
-        "server": None,
-        "client2": EndPoint(host="localhost", port="51820"),
-    }
+    wg = WireguardMonitor(
+        interfaces={
+            "client1": str(WIREGUARD_CONFIG_DIR / "client1.conf"),
+            "client2": str(WIREGUARD_CONFIG_DIR / "client2.conf"),
+        }
+    )
+    assert wg.interfaces == [
+        WireGuardInterface(
+            interface_spec="client1", endpoint=EndPoint(host="localhost", port=8888)
+        ),
+        WireGuardInterface(
+            interface_spec="client2", endpoint=EndPoint(host="localhost", port=51820)
+        ),
+    ]
+
+
+def test_read_wg_status_server_and_client_config():
+    output = (WIREGUARD_CONFIG_DIR / "wg-show-output1.txt").read_text()
+    with patch("subprocess.run") as mock_subprocess_run:
+        mock_subprocess_run.return_value = MagicMock(stdout=output)
+        delta = WireguardMonitor.time_since_last_handshake(interface="wg-client")
+        assert delta > 1000
