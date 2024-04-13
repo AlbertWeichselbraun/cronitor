59aff5ab47196dcc446ad0b5d034e56fd1848920
Albert Weichselbraun <albert@weichselbraun.net>
2024-04-13 14:45:42 +0200

    chg: move cli client to a separate package.

--- src/cronvisio/cli.py
+++ src/cronvisio/cli.py
@@ -13,37 +13,49 @@ from cronvisio.monitor.borgbackup import BorgBackupMonitor
 from cronvisio.monitor.tlsreport import TLSReportMonitor
 from cronvisio.notifier.matrix import MatrixNotifier
 
-if len(sys.argv) < 2:
-    print(f'{sys.argv[0]} [hourly|daily|weekly] [nonotify]')
-    sys.exit(-1)
 
-config = load(open('cronvisio.json'))
-
-# optional: disable notifications for debuging
-if len(sys.argv) > 2 and sys.argv[2] == 'nonotify':
-    from cronvisio.notifier.stdout import StdoutNotifier
-    update_notifiers = [StdoutNotifier()]
-    delight_notifier = [StdoutNotifier()]
-else:
-    update_notifiers = [MatrixNotifier(**config['matrix']['updates'])]
-    delight_notifier = [MatrixNotifier(**config['matrix']['delight'])]
-
-match sys.argv[1]:
-    case 'hourly':
-        # postfix
-        #monitors = [PostfixMonitor()]
-        #Cronitor.cronvisio(monitors, update_notifiers)
-        # kindle
-        monitors = [AmazonKindleQuotes(**config['amazon_kindle_quotes'])]
-        Cronitor.cronitor(monitors, delight_notifier)
-    case 'daily':
-        monitors = [TLSReportMonitor(**config['tlsreport_monitor']),
-                    AutoMysqlBackup(**config['automysqlbackup_monitor'])]
-        Cronitor.cronitor(monitors, update_notifiers)
-    case 'weekly':
-        monitors = [PostfixMonitor(), TLSReportMonitor(**config['tlsreport_monitor']),
-                    BorgBackupMonitor(**config['borgbackup_monitor'])]
-        Cronitor.cronitor(monitors, notifiers=update_notifiers, force=True)
-    case _:
-        print(f'Unsupported parameter {sys.argv[1]}.')
+def cli():
+    if len(sys.argv) < 2:
+        print(f"{sys.argv[0]} [hourly|daily|weekly] [nonotify]")
         sys.exit(-1)
+
+    config = load(open("cronvisio.json"))
+
+    # optional: disable notifications for debuging
+    if len(sys.argv) > 2 and sys.argv[2] == "nonotify":
+        from cronvisio.notifier.stdout import StdoutNotifier
+
+        update_notifiers = [StdoutNotifier()]
+        delight_notifier = [StdoutNotifier()]
+    else:
+        update_notifiers = [MatrixNotifier(**config["matrix"]["updates"])]
+        delight_notifier = [MatrixNotifier(**config["matrix"]["delight"])]
+
+    match sys.argv[1]:
+        case "hourly":
+            # postfix
+            # monitors = [PostfixMonitor()]
+            # Cronitor.cronvisio(monitors, update_notifiers)
+            # kindle
+            monitors = [AmazonKindleQuotes(**config["amazon_kindle_quotes"])]
+            Cronitor.cronitor(monitors, delight_notifier)
+        case "daily":
+            monitors = [
+                TLSReportMonitor(**config["tlsreport_monitor"]),
+                AutoMysqlBackup(**config["automysqlbackup_monitor"]),
+            ]
+            Cronitor.cronitor(monitors, update_notifiers)
+        case "weekly":
+            monitors = [
+                PostfixMonitor(),
+                TLSReportMonitor(**config["tlsreport_monitor"]),
+                BorgBackupMonitor(**config["borgbackup_monitor"]),
+            ]
+            Cronitor.cronitor(monitors, notifiers=update_notifiers, force=True)
+        case _:
+            print(f"Unsupported parameter {sys.argv[1]}.")
+            sys.exit(-1)
+
+
+if __name__ == "__main__":
+    cli()
